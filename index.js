cb8ab14da85d2c269cbe236de08d9ef6a2fe1019
Galen Lim <jingliang.lim@gmail.com>
2021-06-24 12:27:11 +0800

    Ex 3.3 - Add display of single entry

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${timeReceived}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find((person) => person.id === id)
+
+    if (person) {
+        return response.json(persons)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
