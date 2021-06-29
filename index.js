1bbac627874be4f326f4ec9c5f65e59f50f7b255
Galen Lim <jingliang.lim@gmail.com>
2021-06-29 19:18:07 +0800

    Ex 3.9 - Connect frontend with backend locally

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -7,6 +8,7 @@ morgan.token('body', (req) => {
     return JSON.stringify(req.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -76,7 +78,7 @@ app.post('/api/persons', (request, response) => {
     if (persons.find(existingPerson => existingPerson.name === person.name)) {
         return errorMessage('name must be unique')
     }
-    
+
     person.id = Math.floor(Math.random() * 10000)
     persons = persons.concat(person)
 
