a5d395fd950ce072fed9c6cba33d391cb910713d
Galen Lim <jingliang.lim@gmail.com>
2021-07-05 16:33:55 +0800

    Ex 3.13 - Add database module for fetching entries

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person.js')
 
 const app = express()
 
@@ -14,11 +16,6 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
     {
         "name": "Ada Lovelace",
         "number": "39-44-5323523",
@@ -37,7 +34,9 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
