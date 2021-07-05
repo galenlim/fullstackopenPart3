66206fecba2ad84f059650a87d94b409ad199e82
Galen Lim <jingliang.lim@gmail.com>
2021-07-06 01:03:05 +0800

    Ex 3.16 - Add error handling with middleware

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     const errorMessage = (message) => response.status(400).json( { error: `${message}` } )
@@ -94,11 +94,21 @@ app.post('/api/persons', (request, response) => {
         .then(savedPerson => {
             response.json(savedPerson)
         })
-        .catch(error => {
-            console.log(error)
-        })
+        .catch(error => next(error))
 
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
