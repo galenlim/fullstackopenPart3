9b5c844b4fc6bf11496785405b76e0c22d76f1e0
Galen Lim <jingliang.lim@gmail.com>
2021-07-06 11:11:04 +0800

    Ex 3.17 - Modify backend to support update of existing persons

--- index.js
+++ index.js
@@ -98,6 +98,22 @@ app.post('/api/persons', (request, response, next) => {
 
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+
+})
+
+
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
